bb1c357be760cbfca916bec141aa414e2801e344
abdullah ayyaz <abdullah.ayyaz66@gmail.com>
2020-04-10 18:59:49 +0100

    finessed the ting

---
+++ Test-Env/cookbooks/IT-Job-Task/spec/unit/recipes/default_spec.rb
@@ -0,0 +1,36 @@
+#
+# Cookbook:: IT-Job-Task
+# Spec:: default
+#
+# Copyright:: 2020, The Authors, All Rights Reserved.
+
+require 'spec_helper'
+
+describe 'IT-Job-Task::default' do
+  context 'When all attributes are default, on Ubuntu 18.04' do
+    # for a complete list of available platforms and versions see:
+    # https://github.com/chefspec/fauxhai/blob/master/PLATFORMS.md
+    platform 'ubuntu', '18.04'
+
+    it 'runs apt get update' do
+      expect(chef_run).to update_apt_update 'update_sources'
+    end
+
+    it 'should include recipe poise-python' do
+      expect(chef_run).to install_package 'python3-pip'
+    end
+
+    it 'should create a Downloads folder' do
+      expect(chef_run).to create_directory '/home/vagrant/Downloads'
+    end
+
+    it 'should create a remote Downloads folder' do
+      expect(chef_run).to create_remote_directory '/home/vagrant/app'
+    end
+
+    it 'converges successfully' do
+      expect { chef_run }.to_not raise_error
+    end
+
+  end
+end
